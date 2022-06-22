9aabe6f486a8d4dcbc608d8a0008fea3759f3233
gayatrijha <jha.gayatri01@gmail.com>
2022-06-22 21:13:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -17,7 +17,7 @@ const  App =()=> {
 
     return (
       <div>
-        <BrowserRouter>
+        <HashRouter>
           <Navbar/>
           <LoadingBar
             height={3}
@@ -31,10 +31,12 @@ const  App =()=> {
             <Route exact path='/business' element= { <News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/> }/>
             <Route exact path='/health' element= { <News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"/> }/>
             <Route exact path='/science' element= { <News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/> }/>
+           
+            <Route exact path='/general' element= { <News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="general"/> }/>
             <Route exact path='/sports' element= { <News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/> }/>
             <Route exact path='/technology' element= { <News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/> }/>
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
 
       </div>
     )
